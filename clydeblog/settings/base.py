ff0b6ce7d9e89dc4d8ed064e56873e5bfe26040d
clyde91 <clyde91@163.com>
2021-02-18 23:45:52 +0800

    0.99q

--- clydeblog/settings/base.py
+++ clydeblog/settings/base.py
@@ -222,11 +222,4 @@ NUM_ARTICLE_PER_PAGE = 8  # 每页显示文章数
 SITE_ID = 1
 
 
-# 邮箱
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = False   # 是否使用TLS安全传输协议(用于在两个通信应用程序之间提供保密性和数据完整性。)
-EMAIL_USE_SSL = True    # 是否使用SSL加密，qq企业邮箱要求使用
-EMAIL_HOST = 'smtp.qq.com'   # 发送邮件的邮箱 的 SMTP服务器，这里用了163邮箱
-EMAIL_PORT = 465     # 发件箱的SMTP服务器端口
-EMAIL_HOST_USER = '66704470@qq.com'    # 发送邮件的邮箱地址
-EMAIL_HOST_PASSWORD = 'rysyfshfxfstbhfe'         # 发送邮件的邮箱密码(这里使用的是授权码)
+
