9f7ceebe00fbee58a7da4ff3ab19a1f66aed14f5
clyde91 <clyde91@163.com>
2021-02-23 15:53:27 +0800

    1.00a

--- arch_blog/views.py
+++ arch_blog/views.py
@@ -13,21 +13,21 @@ now_app_name = "建筑"
 
 
 def blog_article(request, id):
-    article = get_object_or_404(ArchBlog, id=id)  #通过content_type和id获得的obj
-    key=read_click(request, obj=article)
+    article = get_object_or_404(ArchBlog, id=id)  # 通过content_type和id获得的obj
+    key = read_click(request, obj=article)
     context = {}
     context['article'] = article
     context['object_id'] = id
     content_type = ContentType.objects.get_for_model(article)  # 用具体的obj来获得content_type
     comments = Comment.objects.filter(content_type=content_type, object_id=id, parent=None)  #只筛选根评论（根据parent=none）
     context['comments'] = comments
-    context['comment_user'] = get_object_or_404(User, id=1).profile.nickname
     context['comment_form'] = CommentForm(initial={"content_type":content_type.model, "object_id":id, "reply_comment_id":"0"})  # 实例化一个form表单。content_type.model这个.model很关键不然会出错
     context["now_categorys"] = Category.objects.all()  # 当前所有分类
     context['now_app_name'] = now_app_name
     response = render(request,"article_detail.html", context)    # 响应
     response.set_cookie(key, max_age=1200,)    # 给字典赋值真
     return response
+
 
 def blog_list(request):
     context = {}
@@ -42,8 +42,8 @@ def blog_list(request):
 
 def blog_category(request, id):
     context = {}
-    category = get_object_or_404(Category,id=id)    #通过id=id获取分类的实例（当前category）
-    articles_category = ArchBlog.objects.filter(category=id)    #用分类筛选后的文章
+    category = get_object_or_404(Category,id=id)    # 通过id=id获取分类的实例（当前category）
+    articles_category = ArchBlog.objects.filter(category=id)    # 用分类筛选后的文章
     paginate(request,articles_all=articles_category,context=context)    # 分页器
     context["articles"] = articles_category  # 当前分类的所有文章
     context["now_category"] = category  # 当前分类
--- static/ckeditor/ckeditor/config.js
+++ static/ckeditor/ckeditor/config.js
@@ -7,5 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-    config.font_names = config.font_names + '微软雅黑;宋体;黑体;仿宋_GB2312;楷体_GB2312;隶书;幼圆;' ;
+    config.font_names = config.font_names + '微软雅黑;宋体;黑体;仿宋_GB2312;楷体_GB2312;隶书;幼圆;';
+    config.disallowedContent = 'img{width,height};img[width,height]';
 };
