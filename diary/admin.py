acf9218693bd9a153d6a4ca5f2eea0eed277461a
clyde91 <clyde91@163.com>
2021-03-11 13:17:36 +0800

    1.05b

--- diary/admin.py
+++ diary/admin.py
@@ -4,7 +4,7 @@ from .models import Diary
 
 @admin.register(Diary)
 class DiaryAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "created_time", "modified_time", "get_read_num")
+    list_display = ("id", "title", "created_time", "modified_time", "get_read_num", "private")
     list_per_page = 20
     ordering = ("id",)
     list_display_links = ("title",)    # 打开链接的字段
