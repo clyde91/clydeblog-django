c37161459e88a0019d42f57c5ac1f1807736afc8
clyde91 <clyde91@163.com>
2021-03-11 13:15:11 +0800

    1.05a

--- diary/admin.py
+++ diary/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Diary, Private
+from .models import Diary
 
 
 @admin.register(Diary)
@@ -9,12 +9,3 @@ class DiaryAdmin(admin.ModelAdmin):
     ordering = ("id",)
     list_display_links = ("title",)    # 打开链接的字段
     # fields = ("title","body")    # 在修改页面显示的字段数
-
-
-@admin.register(Private)
-class PrivateAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "created_time", "modified_time", "get_read_num")
-    list_per_page = 20
-    ordering = ("id",)
-    list_display_links = ("title",)    # 打开链接的字段
-
